d114a9ee656a6e230243ffe5e5245e456d29ea4a
Shadab Ali <shadabali604@gmail.com>
2021-01-19 19:10:19 +0530

    added: completed

--- block-BRaaaa/code/script.js
+++ block-BRaaaa/code/script.js
@@ -2,33 +2,45 @@ let userInput = document.querySelector("#userInput");
 let content = document.querySelector(".content");
 let moviesUI = document.createElement("ul");
 moviesUI.setAttribute("class", "movies");
-let movieList = [];
-let index = 0;
+let moviesList = JSON.parse(localStorage.getItem("moviesList")) ?? [];
 function createUI(movies) {
   moviesUI.innerHTML = "";
-  movies.forEach((movie) => {
+  movies.forEach((movie, index) => {
     let movieUI = document.createElement("li");
     movieUI.setAttribute("class", "movie");
-    movieUI.innerText = movie.movieName;
+    movieUI.setAttribute("data-id", index);
+    movieUI.innerHTML = `<span> ${movie.movieName}</span>`;
     let status = document.createElement("span");
-    status.setAttribute = ("class", "status");
-    status.innerText = "x";
-    status.setAttribute = "data-index";
+    status.setAttribute("class", "status");
+    status.textContent = movie.isWatch ? "Watched" : "Watch";
+    status.setAttribute("data-index", index);
     movieUI.appendChild(status);
     moviesUI.appendChild(movieUI);
     content.appendChild(moviesUI);
   });
+  localStorage.setItem("moviesList", JSON.stringify(moviesList));
 }
 
 userInput.addEventListener("keyup", function (event) {
-  if (event.code === "Enter") {
+  if (event.keyCode == 13 && event.target.value.trim()) {
     let text = event.target.value;
-    movieList.push({
+    moviesList.unshift({
       movieName: text,
-      status: false,
+      isWatch: false,
     });
-    index++;
-    createUI(movieList, index);
     event.target.value = "";
+    createUI(moviesList);
   }
 });
+function updateStatus(index) {
+  moviesList[index].isWatch = !moviesList[index].isWatch;
+  localStorage.setItem("moviesList", JSON.stringify(moviesList));
+  createUI(moviesList);
+}
+
+content.addEventListener("click", (event) => {
+  if (event.target.classList.contains("status")) {
+    updateStatus(event.target.dataset.index);
+  }
+});
+createUI(moviesList);
