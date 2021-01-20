fb99c7654411db3d36af566d4e4a969cb341eae0
Shadab Ali <shadabali604@gmail.com>
2021-01-20 10:43:19 +0530

    updated: js

--- block-BRaaae/code/script.js
+++ block-BRaaae/code/script.js
@@ -3,17 +3,45 @@ let content = document.querySelector(".content");
 let moviesUI = document.createElement("ul");
 moviesUI.setAttribute("class", "movies");
 let moviesList = JSON.parse(localStorage.getItem("moviesList")) ?? [];
+function elt(type, attr = {}, ...children) {
+  let el = document.createElement(type);
+  for (const key in attr) {
+    if (key.startsWith("data-")) {
+      el.setAttribute(key, attr[key]);
+    } else if (key.startsWith("class")) {
+      el.classList.add(attr[key]);
+    } else {
+      el[key] = attr[key];
+    }
+  }
+  children.forEach((child) => {
+    if (typeof child === "object") {
+      el.append(child);
+    } else {
+      const text = document.createTextNode(child);
+      el.append(text);
+    }
+  });
+  return el;
+}
 function createUI(movies) {
   moviesUI.innerHTML = "";
+
   movies.forEach((movie, index) => {
-    let movieUI = document.createElement("li");
-    movieUI.setAttribute("class", "movie");
-    movieUI.setAttribute("data-id", index);
+    let status = elt(
+      "span",
+      { class: "status", "data-index": index },
+      movie.isWatch ? "Watched" : "Watch"
+    );
+    let movieUI = elt(
+      "li",
+      {
+        class: "movie",
+        "data-id": index,
+      },
+      status
+    );
     movieUI.innerHTML = `<span> ${movie.movieName}</span>`;
-    let status = document.createElement("span");
-    status.setAttribute("class", "status");
-    status.textContent = movie.isWatch ? "Watched" : "Watch";
-    status.setAttribute("data-index", index);
     movieUI.appendChild(status);
     moviesUI.appendChild(movieUI);
     content.appendChild(moviesUI);
