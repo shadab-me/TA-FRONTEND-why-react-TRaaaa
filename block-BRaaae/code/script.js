d398efa2f914b650d57a1770c379f534126dd393
Shadab Ali <shadabali604@gmail.com>
2021-01-21 10:39:40 +0530

    done

--- block-BRaaae/code/script.js
+++ block-BRaaae/code/script.js
@@ -1,8 +1,10 @@
 let userInput = document.querySelector("#userInput");
 let content = document.querySelector(".content");
+let moviesUI = document.createElement("ul");
+moviesUI.setAttribute("className", "movies");
 let moviesList = JSON.parse(localStorage.getItem("moviesList")) ?? [];
-
-/*function elem(type, attr = {}, ...children) {
+/*
+function elt(type, attr = {}, ...children) {
   let el = document.createElement(type);
   for (const key in attr) {
     if (key.startsWith("data-")) {
@@ -23,27 +25,31 @@ let moviesList = JSON.parse(localStorage.getItem("moviesList")) ?? [];
   });
   return el;
 }
+
 */
+
+let elt = React.createElement;
+
 function createUI(movies) {
-  let allMovies = movies.map((movie, index) => {
-    let status = React.createElement(
+  let moviesUI = movies.map((movie, index) => {
+    console.log(movie.movieName);
+    let status = elt(
       "span",
       { className: "status", "data-index": index },
       movie.isWatch ? "Watched" : "Watch"
     );
-    let movieUI = React.createElement(
+    let movieUI = elt(
       "li",
       {
         className: "movie",
         "data-id": index,
       },
+      movie.movieName,
       status
     );
-    movieUI.innerHTML = `<span> ${movie.movieName}</span>`;
-    let moviesUI = React.createElement("ul", { className: "movies" }, movieUI);
-    return moviesUI;
+    return movieUI;
   });
-  ReactDOM.render(allMovies, content);
+  ReactDOM.render(moviesUI, document.getElementById("root"));
   localStorage.setItem("moviesList", JSON.stringify(moviesList));
 }
 
@@ -64,7 +70,7 @@ function updateStatus(index) {
   createUI(moviesList);
 }
 
-content.addEventListener("click", (event) => {
+document.getElementById("root").addEventListener("click", (event) => {
   if (event.target.classList.contains("status")) {
     updateStatus(event.target.dataset.index);
   }
