4dcb07d508e9d4b88523677e6d8572437c7fae2d
Shadab Ali <shadabali604@gmail.com>
2021-01-26 07:23:21 +0530

    added: completed

--- block-BRaaat/code/countries/src/Countries.js
+++ block-BRaaat/code/countries/src/Countries.js
@@ -1,5 +1,11 @@
 import countries from "./config/countries.json";
 import "./Countries.css";
+function clickHandler(country) {
+  alert(
+    `country Name is ${country.name} and country emoji is ${country.emoji}...`
+  );
+}
+
 function Countries() {
   return countries.map((country) => {
     return (
@@ -7,7 +13,14 @@ function Countries() {
         <div className="card">
           <h2 style={{ width: "100%" }}>{country.emoji}</h2>
           <div className="container">
-            <h4>{country.name}</h4>
+            <h4
+              className="name"
+              onClick={() => {
+                clickHandler(country);
+              }}
+            >
+              {country.name}
+            </h4>
           </div>
         </div>
       </div>
